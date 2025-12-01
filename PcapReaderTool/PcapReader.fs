caacdf7bc37b9822617702252cb9578d8cc1e100
vgerlingsson <243281673+vgerlingsson@users.noreply.github.com>
2025-12-01 09:02:50 +0100

    Fix address handling

--- PcapReaderTool/PcapReader.fs
+++ PcapReaderTool/PcapReader.fs
@@ -12,8 +12,8 @@ type PcapFromCsv =
       Length: string
       Info: string }
 
-let filePath = @"../TestExamplePcapCsv.csv"
-let outputPath = @"../TestExamplePcapCsv_Anonymized.csv"
+let filePath = @"../CSVpcapTest.csv"
+let outputPath = @"../CSVpcapTest_Anonymized.csv"
 
 let originalLines = File.ReadAllLines filePath
 
@@ -35,15 +35,87 @@ printfn $"Found {distinctAddresses.Length} distinct addresses."
 
 let random = Random 42
 
+let isMac (address: string) =
+    let sep =
+        if address.Contains(":") then Some ":"
+        elif address.Contains("-") then Some "-"
+        else None
+
+    match sep with
+    | None -> false
+    | Some sep ->
+        let parts = address.Split(sep)
+        parts.Length = 6 &&
+        parts |> Array.forall (fun p ->
+            p<> "" &&
+            p.Length <= 2 &&
+            p |> Seq.forall (fun c -> Char.IsDigit(c) || "abcdefABCDEF".Contains(c)))
+
+let isIpv6 (address: string) =
+    address.Contains(":") && not (isMac address)
+
 let generateReplacement (original: string) =
-    if original.Contains(".") then //ipv4
-        $"{random.Next(1, 255)}.{random.Next(1, 255)}.{random.Next(1, 255)}.{random.Next(1, 255)}"
-    elif original.Contains("::") || original.Split(':').Length > 6 then //ipv6
-        let parts = Array.init 8 (fun _ -> random.Next(0, 65535).ToString("x4"))
-        String.Join(":", parts)
-    elif original.Split(':').Length = 6 then //MAC addresses
-        let parts = Array.init 6 (fun _ -> random.Next(0, 255).ToString("X2"))
-        String.Join(":", parts)
+    if original.Contains(".") then // ipv4
+        let parts = original.Split('.')
+        let anon =
+            parts
+            |> Array.map (fun p ->
+                let width = p.Length
+                random.Next(0, 255).ToString().PadLeft(width, '0')
+            )
+        String.Join(".", anon)
+
+    elif isIpv6 original then // ipv6
+        if original.Contains("::") then // compressed ipv6
+            let parts = original.Split("::")
+            let left =
+                if parts.[0] = "" then [||]
+                else parts.[0].Split(':')
+            let right =
+                if parts.Length = 1 || parts.[1] = "" then [||]
+                else parts.[1].Split(':')
+
+            let leftAnon =
+                left
+                |> Array.map (fun _ -> random.Next(0, 65535).ToString("x"))
+
+            let rightAnon =
+                right
+                |> Array.map (fun _ -> random.Next(0, 65535).ToString("x"))
+
+            match leftAnon, rightAnon with
+            | [||], [||] -> "::"
+            | [||], r -> "::" + String.Join(":", r)
+            | l, [||] -> String.Join(":", l) + "::"
+            | l, r -> String.Join(":", l) + "::" + String.Join(":", r)
+
+        else // full ipv6
+            let parts = original.Split(':')
+            let anon =
+                parts
+                |> Array.map (fun _ -> random.Next(0, 65535).ToString("x"))
+            String.Join(":", anon)
+    elif isMac original then
+        let sep =
+            if original.Contains(":") then "-"
+            else "-"
+        let parts = original.Split(sep)
+        let anon =
+            parts
+            |> Array.map(fun p ->
+                let hex = random.Next(0, 255).ToString("X2")
+                
+                if (p.ToUpper() = p) then hex
+                elif p.ToLower() = p then hex.ToLower()
+                else
+                    let chars = hex.ToCharArray()
+                    for i in 0 .. min (p.Length - 1) (chars.Length - 1) do
+                        if Char.IsLower(p.[i]) then
+                            chars.[i] <- Char.ToLower(chars.[i])
+                    String(chars)
+            )
+
+        String.Join(sep, anon)
     else
         original
 
